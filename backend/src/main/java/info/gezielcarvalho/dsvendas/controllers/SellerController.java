d06ef814c8bf495ba39b6c7c9d7703a7f22bffc2
Geziel Carvalho <geziel.natal@gmail.com>
2021-09-08 23:02:01 -0300

    Layers

---
+++ backend/src/main/java/info/gezielcarvalho/dsvendas/controllers/SellerController.java
@@ -0,0 +1,26 @@
+package info.gezielcarvalho.dsvendas.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import info.gezielcarvalho.dsvendas.dto.SellerDTO;
+import info.gezielcarvalho.dsvendas.services.SellerService;
+
+@RestController
+@RequestMapping(value = "/sellers")
+public class SellerController {
+	
+	@Autowired
+	private SellerService service;
+	
+	@GetMapping
+	public ResponseEntity<List<SellerDTO>> findaAll(){
+		List<SellerDTO> list = service.findAll();
+		return ResponseEntity.ok(list);
+	}
+}
---
+++ backend/src/main/java/info/gezielcarvalho/dsvendas/repositories/SellerRepository.java
@@ -0,0 +1,9 @@
+package info.gezielcarvalho.dsvendas.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import info.gezielcarvalho.dsvendas.entities.Seller;
+
+public interface SellerRepository extends JpaRepository<Seller, Long>{
+
+}
